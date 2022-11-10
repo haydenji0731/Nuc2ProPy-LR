18dd784ef6f0416559ac34be8df69ae65735e71f
Hayden Ji <haydenji@haydens-mbp.acasmart.jh.edu>
2022-11-10 10:03:22 -0500

    implement alignment

--- align.py
+++ align.py
@@ -1,6 +1,30 @@
 # import packages
+import numpy
+import findOrf
 
-# def smith_waterman_align(query_aa, target_db):
+# def kmer_prefilter(aa_seq, prot_db):
+#     prot_db.
+
+
+def smith_waterman_align(x, y, s):
+    V = numpy.zeros(len(x) + 1, len(y) + 1, dtype=int)
+    for i in range(1, len(x) + 1):
+        for j in range(1, len(y) + 1):
+            V[i, j] = max(V[i - 1, j - 1] + s(x[i - 1], y[j - 1]),  # diagonal
+                          V[i - 1, j] + s(x[i - 1], '-'),  # vertical
+                          V[i, j - 1] + s('-', y[j - 1]),  # horizontal
+                          0)
+    argmax = numpy.where(V == V.max())
+    return V, int(V[argmax])
+
+
+def compute_cost(xc, yc):
+    if xc == yc:
+        return 1
+    # uniform gap cost (open / extend)
+    if xc == '-' or yc == '-':
+        return -1
+    return -1
 
 
 def main():
