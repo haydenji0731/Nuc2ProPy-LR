20036abd3c3c0c21edf6193a7dcfeddf3b77ecf7
Hayden Ji <haydenji@Haydens-MacBook-Pro-2.local>
2022-11-13 16:25:00 -0500

    Modify buildIndex and fixing interpreter errors

--- align.py
+++ align.py
@@ -1,35 +1,55 @@
 # import packages
 import numpy
-import findOrf
+import buildIndex
+import sys
+import pyfastx
 
-# def kmer_prefilter(aa_seq, prot_db):
-#     prot_db.
+
+def kmer_prefilter(aa_seq, prot_db, kmer_size):
+    # TODO: implement consecutive diagonal matches?
+    prefilter_set = set()
+    for i in range(0, len(aa_seq) - kmer_size + 1):
+        kmer = aa_seq[i:i + kmer_size]
+        if kmer in prot_db.contains(kmer):
+            # merge 2 sets
+            updated_set = prefilter_set | prot_db[kmer]
+
+    # diagonal double match prefilter
 
 
-def smith_waterman_align(x, y, s):
+
+
+
+
+
+def smith_waterman_align(x, y, cost_func):
     V = numpy.zeros(len(x) + 1, len(y) + 1, dtype=int)
     for i in range(1, len(x) + 1):
         for j in range(1, len(y) + 1):
-            V[i, j] = max(V[i - 1, j - 1] + s(x[i - 1], y[j - 1]),  # diagonal
-                          V[i - 1, j] + s(x[i - 1], '-'),  # vertical
-                          V[i, j - 1] + s('-', y[j - 1]),  # horizontal
+            V[i, j] = max(V[i - 1, j - 1] + cost_func(x[i - 1], y[j - 1]),  # diagonal
+                          V[i - 1, j] + cost_func(x[i - 1], '-'),  # vertical
+                          V[i, j - 1] + cost_func('-', y[j - 1]),  # horizontal
                           0)
     argmax = numpy.where(V == V.max())
     return V, int(V[argmax])
 
 
-def compute_cost(xc, yc):
+def compute_cost(xc, yc, go, ge, sub, match, is_open):
     if xc == yc:
-        return 1
-    # uniform gap cost (open / extend)
+        return match
     if xc == '-' or yc == '-':
-        return -1
-    return -1
+        if is_open:
+            return ge
+        else:
+            return go
+    return sub
 
 
-def main():
-    print("implement sw alignment")
+def main(input_file, kmer_size):
+    proteins = pyfastx.Fastx(input_file)
+    prot_db = buildIndex.db_index(kmer_size, proteins)
+
 
 
 if __name__ == '__main__':
-    main()
+    main(sys.argv[1], int(sys.argv[2]))
--- buildIndex.py
+++ buildIndex.py
@@ -3,36 +3,44 @@ import pyfastx
 import sys
 import time
 
-num_seq = 0
-
 
 class db_index:
-    def __init__(self, kmer_size):
+    # overload the constructor
+    def __init__(self, kmer_size, proteins=None):
         self.kmer_table = dict()
         self.kmer_size = kmer_size
+        self.num_seq = len(proteins)
+        if proteins:
+            for protein in proteins:
+                print(protein.seq)
+                self.insert(protein)
 
     def insert(self, protein):
-        name = protein[0]
-        seq = protein[1]
-        for i in range(0, len(seq) - self.kmer_size + 1):
-            kmer = seq[i:i + 5]
-            if kmer in self.kmer_table.keys():
-                self.kmer_table[kmer].add(name)
+        seq = protein.seq
+        for i in range(0, len(protein.seq) - self.kmer_size + 1):
+            kmer = protein.seq[i:i + self.kmer_size]
+            if self.contains(kmer):
+                # target seq identifier, pos
+                self.kmer_table[kmer].add((protein.id, i))
             else:
-                name_set = {name}
+                name_set = {(protein.id, i)}
                 self.kmer_table[kmer] = name_set
+
+    def contains(self, kmer):
+        if kmer in self.kmer_table.keys():
+            return True
+        return False
 
 
 def main(input_file, kmer_size, out_dir, out_file):
     start = time.time()
-    proteins = pyfastx.Fastx(input_file)
-    prot_db = db_index(kmer_size)
-    global num_seq
-    for protein in proteins:
-        num_seq += 1
-        prot_db.insert(protein)
-    duration = time.time() - start
-    print("Processed %.0f sequences in %.2fs" % (num_seq, duration))
+    # build_index = True by default
+    proteins = pyfastx.Fasta(input_file)
+    prot_db = db_index(kmer_size, proteins)
+    # prot_db_alt = db_index(kmer_size)
+    # print(prot_db_alt.num_seq)
+    # duration = time.time() - start
+    # print("Processed %.0f sequences in %.2fs" % (prot_db.num_seq, duration))
 
 
 if __name__ == '__main__':
